fd796f5276b762b215da45149597b72750f989b2
oharsta <oharsta@zilverline.com>
2014-08-19 13:12:19 +0200

    Added demo link

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 var paper = Snap("#svg");
 
 (function start() {
+    var svg = document.getElementById("svg");
+    svg.style.width = window.innerWidth;
+    svg.style.height = window.innerHeight;
     paper.circle(Math.random() * screen.width, Math.random() * screen.height, Math.random() * 500 )
          .attr({fill: '#' + Math.random().toString(16).substring(4)})
-         .animate({r: Math.random() * 200}, 500);  
-    setTimeout(start, 500);
+         .animate({r: Math.random() * 200}, 100);  
+    setTimeout(start, 100);
 })();
